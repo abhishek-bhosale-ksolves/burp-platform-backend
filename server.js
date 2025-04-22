688f25626656dbedef7d324929f8851afe700ef2
AbhishekASLK <abhishek.bhosale@ksolves.com>
2025-04-22 15:04:23 +0530

    removed unnessasary cookies code

--- server.js
+++ server.js
@@ -37,13 +37,6 @@ app.use(
     secret: "my_secret_key",
     resave: false,
     saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None",
-      domain: ".onrender.com",
-      maxAge: 24 * 60 * 60 * 1000,
-    },
   })
 );
 
